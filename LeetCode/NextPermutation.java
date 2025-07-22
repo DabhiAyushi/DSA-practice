f79be436d76a387beb130f78abd074c7ea124b19
DabhiAyushi <78699162+DabhiAyushi@users.noreply.github.com>
2025-07-22 10:54:10 +0530

    Update NextPermutation.java

---
+++ LeetCode/NextPermutation.java
@@ -0,0 +1,41 @@
+//nums = [1,2,3]
+//output = [1,3,2]
+class Solution {
+    public void nextPermutation(int[] nums) {
+
+        int i = nums.length-2;
+
+        //Find first decreasing element from right
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+
+        if(i >= 0){
+
+            int j = nums.length-1;
+
+            while(nums[j] <= nums[i]){
+                j--;
+
+            }
+
+            swap(nums, i, j);   
+        }
+
+            reverse(nums, i+1, nums.length-1);
+        
+    }
+    private void swap(int[] nums, int i, int j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+
+    private void reverse(int[] nums, int start, int end){
+        while(start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
